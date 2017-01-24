a5986877e807701cf292101a33246a5177e026e5
krupskyi <krupskyi.a@gmail.com>
2017-01-24 22:52:00 +0200

    minor fix

--- index.php
+++ index.php
@@ -5,14 +5,12 @@ $rates['USD'] = [];
 $sources['EUR'] = 1;
 $sources['USD'] = 1;
 
-$url = 'http://api.1coindesk.com/v1/bpi/currentprice.json';
-$data = array('key1' => 'value1', 'key2' => 'value2');
+$url = 'http://api.coindesk.com/v1/bpi/currentprice.json';
 
 $options = array(
     'http' => array(
         'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-        'method'  => 'GET',
-        'content' => http_build_query($data)
+        'method'  => 'GET'
     )
 );
 
